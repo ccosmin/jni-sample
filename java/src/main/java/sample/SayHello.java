7fda1344eef23b0829d858bbd2cae41323c9584d
Cosmin Cremarenco <cosmin@snowlinesoftware.com>
2023-06-28 20:17:36 +0200

    Load lib2 then lib1

--- java/src/main/java/sample/SayHello.java
+++ java/src/main/java/sample/SayHello.java
@@ -2,17 +2,21 @@ package sample;
 
 public class SayHello {
   static {
-    //System.loadLibrary("lib1");
-    System.loadLibrary("pathstub");
+    System.loadLibrary("lib2");
+    System.loadLibrary("lib1");
+    //System.loadLibrary("pathstub");
   }
 
   public static void main(String[] args) {
+/*
     if (new SayHello().addToPath("C:\\Users\\cosmin\\devel\\jni-sample\\native\\build\\Debug")) {
       System.loadLibrary("lib1");
       new SayHello().sayHello();
-    }
+*/
+    new SayHello().sayHello();
+    
   }
 
   private native void sayHello();
-  private native boolean addToPath(String str);
+  //private native boolean addToPath(String str);
 }
